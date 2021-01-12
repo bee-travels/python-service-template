635ff4bf8a317c57dfc6853f5ed8004791fafc66
Max Shapiro <magicmax33@gmail.com>
2021-01-12 14:48:13 -0500

    Setup venv

--- {{.ServiceNamePill}}.sh
+++ {{.ServiceNamePill}}.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-if [ "$1" = "start" ]; then
+if [ "$1" = "setup" ]; then
+    python3 -m venv env
+    env/bin/python3 -m pip install --upgrade pip
+    env/bin/pip install -r requirements.txt
+elif [ "$1" = "start" ]; then
     env/bin/python run.py
 elif [ "$1" = "test" ]; then
     env/bin/coverage run -m unittest discover
