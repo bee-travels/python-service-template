1cc0a3c3f22e582df25adf2ee281c7df24d5094a
Max Shapiro <magicmax33@gmail.com>
2021-10-18 16:01:37 -0400

    Omit test files from code coverage

--- {{.ServiceNamePill}}.sh
+++ {{.ServiceNamePill}}.sh
@@ -8,5 +8,5 @@ elif [ "$1" = "start" ]; then
     env/bin/python run.py
 elif [ "$1" = "test" ]; then
     env/bin/coverage run -m unittest discover
-    env/bin/coverage report -m app/*.py app/errors/*.py app/routes/*.py app/services/*.py
+    env/bin/coverage report --omit *test* -m app/*.py app/errors/*.py app/routes/*.py app/services/*.py
 fi
